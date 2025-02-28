1533c307053635f0aaf1de4eaa49cc3aead042e4
NourEssalam <werteni11thug@gmail.com>
2025-02-28 20:30:42 +0100

    test deploy 1

--- src/app/(front-end)/[locale]/not-found.tsx
+++ src/app/(front-end)/[locale]/not-found.tsx
@@ -2,5 +2,9 @@ import { useTranslations } from 'next-intl'
 
 export default function NotFoundPage() {
   const t = useTranslations('Error')
-  return <h1>{t('NotFoundPage')}</h1>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl">{t('NotFoundPage')}</h1>
+    </div>
+  )
 }
