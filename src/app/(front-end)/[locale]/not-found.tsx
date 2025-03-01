c7e62679c48e23267b8923fd2588683512a0972e
NourEssalam <werteni11thug@gmail.com>
2025-03-01 04:30:52 +0100

    add amber color to not found

--- src/app/(front-end)/[locale]/not-found.tsx
+++ src/app/(front-end)/[locale]/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFoundPage() {
   const t = useTranslations('Error')
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl">{t('NotFoundPage')}</h1>
+      <h1 className="text-3xl bg-amber-900">{t('NotFoundPage')}</h1>
     </div>
   )
 }
